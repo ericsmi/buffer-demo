c945be36a11b9733a7d361a4e3cfcbced2ab3635
ericsmi <26430018+ericsmi@users.noreply.github.com>
2026-02-05 01:23:59 -0800

    Update project.v

--- src/project.v
+++ src/project.v
@@ -25,9 +25,9 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-    b b(.A(ui_in[0]),.Y(uo_out[0]));
+    b b(.A(ui_in[0]),.Y(uo_out[7]));
         
-  assign uo_out[7:1]  = 7'd0;  
+  assign uo_out[6:0]  = 7'd0;  
   assign uio_out = 0;
   assign uio_oe  = 0;
 
